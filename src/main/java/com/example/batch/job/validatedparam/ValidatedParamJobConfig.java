45b1ed54622b147e0c02473b4ef964216109ead3
9898s <sueh9898@icloud.com>
2023-10-12 10:16:28 +0900

    feat: 배치 실행 시 파라미터 (파일 이름) 받기 및 (csv) 검증

---
+++ src/main/java/com/example/batch/job/validatedparam/ValidatedParamJobConfig.java
@@ -0,0 +1,70 @@
+package com.example.batch.job.validatedparam;
+
+import com.example.batch.job.validatedparam.validator.FileParamValidator;
+import java.util.Collections;
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.job.CompositeJobParametersValidator;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@RequiredArgsConstructor
+@Configuration
+public class ValidatedParamJobConfig {
+
+  @Autowired
+  private JobBuilderFactory jobBuilderFactory;
+
+  @Autowired
+  private StepBuilderFactory stepBuilderFactory;
+
+  @Bean
+  public Job validatedParamJob(Step validatedParamStep) {
+    return jobBuilderFactory.get("validatedParamJob")
+        .incrementer(new RunIdIncrementer())
+//        .validator(new FileParamValidator())
+        .validator(multipleValidator())
+        .start(validatedParamStep)
+        .build();
+  }
+
+  private CompositeJobParametersValidator multipleValidator() {
+    CompositeJobParametersValidator validator = new CompositeJobParametersValidator();
+    validator.setValidators(Collections.singletonList(new FileParamValidator()));
+
+    return validator;
+  }
+
+  @JobScope
+  @Bean
+  public Step validatedParamStep(Tasklet validatedParamTasklet) {
+    return stepBuilderFactory.get("validatedParamStep")
+        .tasklet(validatedParamTasklet)
+        .build();
+  }
+
+  @StepScope
+  @Bean
+  public Tasklet validatedParamTasklet(@Value("#{jobParameters['fileName']}") String fileName) {
+    return new Tasklet() {
+      @Override
+      public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext)
+          throws Exception {
+        System.out.println("validatedParamTasklet");
+        return RepeatStatus.FINISHED;
+      }
+    };
+  }
+}
